d7fd9ff15b61283b0b92969282dfe7793eeff165
run-ascent <ascentrunclub@gmail.com>
2026-06-26 12:32:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ export default defineConfig({
   plugins: [react()],
   base:
     process.env.VITE_BASE_PATH ||
-    (process.env.NODE_ENV === 'production' ? '/run-ascent/' : '/'),
+    (process.env.NODE_ENV === 'production' ? '/' : '/'),
 });
